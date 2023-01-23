e76bce960c2812258929f732cce49a4f35cba024
Anurag <anuragsinghg99@gmail.com>
2023-01-23 13:10:39 +0530

    Created Restaurant Website UI/UX

---
+++ pages/Menu.js
@@ -0,0 +1,147 @@
+export const MenuSnacks = [
+  {
+    name: "Chole Bathure",
+    price: "60 / Plate",
+  },
+  {
+    name: "Bedmi Puri",
+    price: "60 / Plate",
+  },
+  {
+    name: "Samosa",
+    price: "12",
+  },
+  {
+    name: "Pyaj Kachori",
+    price: "25",
+  },
+  {
+    name: "Desi Ghee Jalebi",
+    price: "360 / KG",
+  },
+  {
+    name: "Paneer Jalebi",
+    price: "400 / KG",
+  },
+  {
+    name: "Dal Kachori",
+    price: "20",
+  },
+  {
+    name: "Dhokla",
+    price: "180 / KG",
+  },
+];
+
+export const MenuChat = [
+  {
+    name: "Gol Gappa",
+    price: "20 / Plate",
+  },
+  {
+    name: "Dahi Gol Gappa",
+    price: "50 / Plate",
+  },
+  {
+    name: "Stuffed Gol Gappa",
+    price: "40 / Plate",
+  },
+  {
+    name: "Tikki",
+    price: "50",
+  },
+  {
+    name: "Bhalla Papdi",
+    price: "50",
+  },
+  {
+    name: "Raj Kachori",
+    price: "60",
+  },
+  {
+    name: "Pao Bhaji",
+    price: "50",
+  },
+  {
+    name: "Dahi Bhalle",
+    price: "50",
+  },
+  {
+    name: "Aloo Chat",
+    price: "40",
+  },
+  {
+    name: "Samosa Chat",
+    price: "50",
+  },
+];
+
+export const MenuChinese = [
+  {
+    name: "Chilli Garlic Chowmein",
+    price: "Half - 60 / Full - 120",
+  },
+  {
+    name: "Paneer Chowmein",
+    price: "Half - 60 / Full - 120",
+  },
+  {
+    name: "Singapuri Chowmein",
+    price: "Half - 60 / Full - 120",
+  },
+  {
+    name: "Chowmein",
+    price: "Half - 50 / Full - 80",
+  },
+  {
+    name: "Chilli Potato",
+    price: "Half - 50 / Full - 80",
+  },
+  {
+    name: "Spring Roll",
+    price: "Half - 30 / Full - 60",
+  },
+  {
+    name: "Manchurian",
+    price: "Half - 50 / Full - 100",
+  },
+  {
+    name: "Hakka Noodles",
+    price: "Half - 70 / Full - 100",
+  },
+  {
+    name: "Honey Chilli Potato",
+    price: "Half - 60 / Full - 100",
+  },
+  {
+    name: "Chilli Paneer",
+    price: "Half - 100 / Full - 160",
+  },
+  {
+    name: "Fry Chowmein",
+    price: "Half - 60 / Full - 100",
+  },
+];
+
+export const MenuPakoda = [
+  {
+    name: "Bread Pakoda",
+    price: "20",
+  },
+  {
+    name: "Paneer Pakada",
+    price: "25",
+  },
+  {
+    name: "Mirchi Pakoda",
+    price: "25",
+  },
+  {
+    name: "Pyaaz Pakoda",
+    price: "25",
+  },
+  {
+    name: "Moong Dal Pakoda",
+    price: "30",
+  },
+];
